c492037be232d08d1e0a64756f074682bb8e2c5d
Onexkka <49119974+Onexkka@users.noreply.github.com>
2019-04-12 00:11:42 +0700

    ver 1.0

---
+++ Game2048/GameModel.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Game2048
+{
+    class GameModel
+    {
+        #region Fields
+        public event Action GridUpdated;
+        public event Action<string> GameOver;
+        public readonly int Size;
+        public int[,] mainGrid;
+
+        private int[,] _backupGrid;
+        private Random _rndIndex;
+        private bool _canUndo = false;
+        private int _maxTile;
+        private int _winTile;
+        private int _turnScore;
+        private int _currentScore;
+        private bool[] _moveLock = new bool[4];
+        #endregion
+
+        #region Cosntructors
+        public GameModel(int size)
+        {
+            Size = size;
+        }
+        #endregion
+
+        #region MainLogic
+        public void InitBoard()
+        {
+            mainGrid = new int[Size, Size];
+
+            _rndIndex = new Random(DateTime.Now.Millisecond);
+            _backupGrid = new int[Size, Size];
+            _canUndo = false;
+
+            _currentScore = 0;
+            _maxTile = 0;
+            _winTile = 64;
+            _turnScore = 0;
+
+            SpawnTile(true);
+            SpawnTile(true);
+
+            Repaint();
+        }
+
+        public void GameStep(KeyEventArgs args)
+        {
+            var keyCode = args.KeyValue;
+
+            switch (keyCode)
+            {
+                case 37:
+                case 38:
+                case 39:
+                case 40:
+                case 87:
+                case 65:
+                case 83:
+                case 68:
+                    CreateGridBackup();
+                    MoveTile(keyCode);
+                    MergeTile(keyCode);
+                    MoveTile(keyCode);
+                    FindGreatestTile();
+                    GameStateAction();
+                    break;
+                case 85: // u
+                    Undo();
+                    Repaint();
+                    break;
+                case 82: // r
+                    InitBoard();
+                    break;
+            }
+        }
+
+        public void MoveTile(int keyCode)
+        {
+            switch (keyCode)
+            {
+                case 37: // 65, 37 A, <
+                case 65:
+                    for (int row = 0; row < Size; row++)
+                        for (int column = 0; column < Size; column++)
+                            if (mainGrid[row, column] == 0)
+                                for (int k = column + 1; k < Size; k++)
+                                    if (mainGrid[row, k] != 0)
+                                    {
+                                        mainGrid[row, column] = mainGrid[row, k];
+                                        mainGrid[row, k] = 0;
+                                        break;
+                                    }
+                    break;
+                case 38: // 87, 38 w ^
+                case 87:
+                    for (int row = 0; row < Size; row++)
+                        for (int column = 0; column < Size; column++)
+                            if (mainGrid[column, row] == 0)
+                                for (int k = column + 1; k < Size; k++)
+                                    if (mainGrid[k, row] != 0)
+                                    {
+                                        mainGrid[column, row] = mainGrid[k, row];
+                                        mainGrid[k, row] = 0;
+                                        break;
+                                    }
+
+                    break;
+                case 39: // 68, 39 D >
+                case 68:
+                    for (int row = 0; row < Size; row++)
+                        for (int column = Size - 1; column >= 0; column--)
+                            if (mainGrid[row, column] == 0)
+                                for (int k = column - 1; k >= 0; k--)
+                                    if (mainGrid[row, k] != 0)
+                                    {
+                                        mainGrid[row, column] = mainGrid[row, k];
+                                        mainGrid[row, k] = 0;
+                                        break;
+                                    }
+
+                    break;
+                case 40: // 83, 40 s !^
+                case 83:
+                    for (int row = 0; row < Size; row++)
+                        for (int column = Size - 1; column >= 0; column--)
+                            if (mainGrid[column, row] == 0)
+                                for (int k = column - 1; k >= 0; k--)
+                                    if (mainGrid[k, row] != 0)
+                                    {
+                                        mainGrid[column, row] = mainGrid[k, row];
+                                        mainGrid[k, row] = 0;
+                                        break;
+                                    }
+                    break;
+            }
+        }
+
+        private void MergeTile(int keyCode)
+        {
+            _turnScore = 0;
+            int mergePairScore;
+
+            switch (keyCode)
+            {
+                case 37: // 65, 37 A, <
+                case 65:
+                    for (int row = 0; row < Size; row++)
+                        for (int column = 0; column < Size - 1; column++)
+                            if (mainGrid[row, column] == mainGrid[row, column + 1])
+                            {
+                                mainGrid[row, column] += mainGrid[row, column + 1];
+                                mainGrid[row, column + 1] = 0;
+                                mergePairScore = CalculateScore(row, column);
+                                _currentScore += mergePairScore;
+                                _turnScore += mergePairScore;
+                            }
+                    if (_turnScore != 0)
+                        Array.Clear(_moveLock, 0, 4);
+                    else
+                        _moveLock[0] = true;
+                    break;
+                case 38:
+                case 87:
+                    for (int row = 0; row < Size; row++)
+                        for (int column = 0; column < Size - 1; column++)
+                            if (mainGrid[column, row] == mainGrid[column + 1, row])
+                            {
+                                mainGrid[column, row] += mainGrid[column + 1, row];
+                                mainGrid[column + 1, row] = 0;
+                                mergePairScore = CalculateScore(column, row);
+                                _currentScore += mergePairScore;
+                                _turnScore += mergePairScore;
+                            }
+                    if (_turnScore != 0)
+                        Array.Clear(_moveLock, 0, 4);
+                    else
+                        _moveLock[1] = true;
+                    break;
+                case 39: // 68, 39 D >
+                case 68:
+                    for (int row = 0; row < Size; row++)
+                        for (int column = Size - 1; column > 0; column--)
+                            if (mainGrid[row, column] == mainGrid[row, column - 1])
+                            {
+                                Array.Clear(_moveLock, 0, 4);
+                                mainGrid[row, column] += mainGrid[row, column - 1];
+                                mainGrid[row, column - 1] = 0;
+                                mergePairScore = CalculateScore(row, column);
+                                _currentScore += mergePairScore;
+                                _turnScore += mergePairScore;
+                            }
+                    if (_turnScore != 0)
+                        Array.Clear(_moveLock, 0, 4);
+                    else
+                        _moveLock[2] = true;
+                    break;
+                case 40: // 83, 40 s !^
+                case 83:
+                    for (int row = 0; row < Size; row++)
+                        for (int column = Size - 1; column > 0; column--)
+                            if (mainGrid[column, row] == mainGrid[column - 1, row])
+                            {
+                                Array.Clear(_moveLock, 0, 4);
+                                mainGrid[column, row] += mainGrid[column - 1, row];
+                                mainGrid[column - 1, row] = 0;
+                                mergePairScore = CalculateScore(column, row);
+                                _turnScore += mergePairScore;
+                                _currentScore += mergePairScore;
+                            }
+                    if (_turnScore != 0)
+                        Array.Clear(_moveLock, 0, 4);
+                    else
+                        _moveLock[3] = true;
+                    break;
+            }
+        }
+
+        private void GameStateAction()
+        {
+            int lockCount = 0;
+            for (int i = 0; i < 4; i++) lockCount += _moveLock[i] ? 1 : 0;
+
+            if (GridFull() && lockCount == 4)
+            {
+                GameOverMessage("LOSE");
+            }
+            else if (GridChanged())
+            {
+                SpawnTile();
+                Repaint();
+                if (_maxTile == _winTile)
+                    GameOverMessage("WIN");
+            }
+        }
+
+        private void CreateGridBackup()
+        {
+            _backupGrid = (int[,])mainGrid.Clone();
+        }
+
+        private void Undo()
+        {
+            if (!_canUndo)
+                return;
+
+            _currentScore -= _turnScore;
+            mainGrid = (int[,])_backupGrid.Clone();
+
+            _canUndo = false;
+        }
+
+        private void FindGreatestTile()
+        {
+            for (int row = 0; row < Size; row++)
+                for (int column = 0; column < Size; column++)
+                    if (mainGrid[row, column] > _maxTile)
+                        _maxTile = mainGrid[row, column];
+        }
+
+        private bool GridFull()
+        {
+            for (int row = 0; row < Size; row++)
+                for (int column = 0; column < Size; column++)
+                {
+                    if (mainGrid[row, column] == 0)
+                        return false;
+                }
+            return true;
+        }
+
+        private bool GridChanged()
+        {
+            for (int i = 0; i < Size; i++)
+                for (int j = 0; j < Size; j++)
+                    if (_backupGrid[i, j] != mainGrid[i, j])
+                    {
+                        _canUndo = true;
+                        return true;
+                    }
+            return false;
+        }
+
+        private void SpawnTile(bool initBlock = false)
+        {
+            int chanceSpawnBigBlock;
+            int row, column;
+
+            row = _rndIndex.Next(Size);
+            column = _rndIndex.Next(Size);
+            do
+            {
+                row = _rndIndex.Next(Size);
+                column = _rndIndex.Next(Size);
+                chanceSpawnBigBlock = _rndIndex.Next(10);
+            } while (mainGrid[row, column] != 0);
+
+            if (chanceSpawnBigBlock < 2 && !initBlock)
+                mainGrid[row, column] = 4;
+            else
+                mainGrid[row, column] = 2;
+        }
+        #endregion
+
+        #region EventInvoke
+        private void Repaint()
+        {
+            GridUpdated?.Invoke();
+        }
+
+        private void GameOverMessage(string msg)
+        {
+            GameOver?.Invoke(msg);
+        }
+        #endregion
+
+        #region FieldsAccses
+        public void SetNewWinTile()
+        {
+            _winTile *= 2;
+        }
+
+        public string GetStats()
+        {
+            return $"Score: {_currentScore}    Win: {_winTile}";
+        }
+
+        public string GetFinalyStats()
+        {
+            return $"Score: {_currentScore}\nMax Tile: {_maxTile}\nWin Tile: { _winTile}\nNext Win Tile: {_winTile * 2}";
+        }
+        #endregion
+
+        #region Helpers
+        private int CalculateScore(int iRow, int iColumn)
+        {
+            return (int)(Math.Log(mainGrid[iRow, iColumn], 2) - 1) * mainGrid[iRow, iColumn];
+        }
+
+        #endregion
+    }
+}
---
+++ Game2048/GameView.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game2048
+{
+    class GameView : Form
+    {
+        GameModel game;
+        TableLayoutPanel table;
+        TableLayoutPanel grid;
+        Label stats;
+
+        private readonly Dictionary<int, Color> _colorDictionary = new Dictionary<int, Color>();
+
+        public GameView(GameModel game)
+        {
+            // Init color Dictionary
+            _colorDictionary.Add(0, Color.FromArgb(51, 63, 77));
+            _colorDictionary.Add(2, Color.FromArgb(129, 128, 215));
+            _colorDictionary.Add(4, Color.FromArgb(162, 90, 214));
+            _colorDictionary.Add(8, Color.FromArgb(38, 105, 221));
+            _colorDictionary.Add(16, Color.FromArgb(33, 185, 213));
+            _colorDictionary.Add(32, Color.FromArgb(0, 202, 155));
+            _colorDictionary.Add(64, Color.FromArgb(67, 207, 23));
+            _colorDictionary.Add(128, Color.FromArgb(247, 192, 1));
+            _colorDictionary.Add(256, Color.FromArgb(245, 129, 20));
+            _colorDictionary.Add(512, Color.FromArgb(255, 84, 61));
+            _colorDictionary.Add(1024, Color.FromArgb(255, 20, 145));
+            _colorDictionary.Add(2048, Color.FromArgb(190, 0, 129));
+            _colorDictionary.Add(4096, Color.FromArgb(20, 255, 255));
+            _colorDictionary.Add(8192, Color.FromArgb(255, 144, 222));
+            _colorDictionary.Add(16384, Color.FromArgb(60, 0, 219));
+            _colorDictionary.Add(32768, Color.FromArgb(255, 207, 171));
+            _colorDictionary.Add(65536, Color.FromArgb(255, 0, 45));
+            _colorDictionary.Add(131072, Color.FromArgb(255, 255, 0));
+
+            // Init Form
+            Text = "Game 2048";
+            BackColor = Color.FromArgb(75, 88, 103);
+
+            this.game = game;
+
+            // Init table 
+            table = new TableLayoutPanel();
+            table.Dock = DockStyle.Fill;
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 10));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 90));
+
+            // Init grid
+            grid = new TableLayoutPanel();
+            grid.Dock = DockStyle.Fill;
+            for (int i = 0; i < game.Size; i++)
+            {
+                grid.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / game.Size));
+                grid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / game.Size));
+            }
+            // cache grid cell 
+            var tiles = new Label[game.Size, game.Size];
+
+            for (int column = 0; column < game.Size; column++)
+                for (int row = 0; row < game.Size; row++)
+                {
+                    var cell = new Label
+                    {
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Dock = DockStyle.Fill,
+                        Margin = new Padding(1)
+                    };
+                    tiles[row, column] = cell;
+                    grid.Controls.Add(cell, column, row);
+                }
+
+            // Init stats
+            stats = new Label()
+            {
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Fill,
+                Font = new Font("Arial", 20F, FontStyle.Bold)
+            };
+
+            // add all element to Form
+            table.Controls.Add(stats);
+            table.Controls.Add(grid);
+            Controls.Add(table);
+
+            // View notify Model (User Input)
+            KeyDown += (sender, args) =>
+            {
+                game.GameStep(args);
+            };
+
+            // Model update View
+            game.GridUpdated += () =>
+            {
+                stats.Text = game.GetStats();
+
+                for (int row = 0; row < game.Size; row++)
+                    for (int column = 0; column < game.Size; column++)
+                    {
+                        var cell = tiles[row, column];
+                        var value = game.mainGrid[row, column];
+                        cell.Text = value.ToString();
+                        if (_colorDictionary.ContainsKey(value))
+                            cell.BackColor = _colorDictionary[value];
+                        else
+                            cell.BackColor = Color.White;
+                    }
+            };
+
+            game.GameOver += (message) =>
+            {
+                var WIN = message.Equals("WIN");
+                stats.Text = WIN ? "You win!" : "You lose!";
+
+                if (MessageBox.Show(game.GetFinalyStats(), WIN ? "Continue ?" : "Try again ?", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    if(WIN)
+                        game.SetNewWinTile();
+                    else
+                        game.SetNewWinTile();
+                }
+                else
+                {
+                    Dispose();
+                }
+            };
+
+            game.InitBoard();
+        }
+
+        static void Main(string[] args)
+        {
+            var size = 8;
+            var cellSize = 100;
+            Application.Run(new GameView(new GameModel(size)) { ClientSize = new Size(size * cellSize, size * cellSize) });
+        }
+    }
+}
